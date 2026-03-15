da84658443cbd93e6f9d2f2e15b29bb36294a04c
manhlinhfs <253059394+manhlinhfs@users.noreply.github.com>
2026-03-15 17:16:37 +0000

    test: type vitest config in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
 import { VitePWA } from "vite-plugin-pwa"
+import { defineConfig } from "vitest/config"
 
 // https://vite.dev/config/
 export default defineConfig({
